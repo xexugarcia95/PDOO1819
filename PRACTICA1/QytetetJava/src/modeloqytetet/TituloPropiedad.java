d3eecd87f74fc3acbf7f08afef4822f1e8d3cfae
xexugarcia95 <xexugarcia95@gmail.com>
2018-09-29 00:33:30 +0200

    .

---
+++ PRACTICA1/QytetetJava/src/modeloqytetet/TituloPropiedad.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloqytetet;
+
+/**
+ *
+ * @author jesus
+ */
+public class TituloPropiedad {
+    private String nombre;
+    private boolean hipotecada;
+    private int precioCompra;
+    private int alquilerBase;
+    private float factorRevalorizacion;
+    private int hipotecaBase;
+    private int precioEdificar;
+    private int numCasas;
+    private int numHoteles;
+
+    public TituloPropiedad(String nombre, boolean hipotecada, int precioCompra, int alquilerBase, float factorRevalorizacion, int hipotecaBase, int precioEdificar) {
+        this.nombre = nombre;
+        this.hipotecada = hipotecada;
+        this.precioCompra = precioCompra;
+        this.alquilerBase = alquilerBase;
+        this.factorRevalorizacion = factorRevalorizacion;
+        this.hipotecaBase = hipotecaBase;
+        this.precioEdificar = precioEdificar;
+        this.hipotecada = false;
+        this.numCasas = 0;
+        this.numHoteles = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isHipotecada() {
+        return hipotecada;
+    }
+
+    public int getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public int getAlquilerBase() {
+        return alquilerBase;
+    }
+
+    public float getFactorRevalorizacion() {
+        return factorRevalorizacion;
+    }
+
+    public int getHipotecaBase() {
+        return hipotecaBase;
+    }
+
+    public int getPrecioEdificar() {
+        return precioEdificar;
+    }
+
+    public int getNumCasas() {
+        return numCasas;
+    }
+
+    public int getNumHoteles() {
+        return numHoteles;
+    }
+
+    public void setHipotecada(boolean hipotecada) {
+        this.hipotecada = hipotecada;
+    }
+
+    @Override
+    public String toString() {
+        return "TituloPropiedad{" + "nombre=" + nombre + ", hipotecada=" + hipotecada + ", precioCompra=" + precioCompra + ", alquilerBase=" + alquilerBase + ", factorRevalorizacion=" + factorRevalorizacion + ", hipotecaBase=" + hipotecaBase + ", precioEdificar=" + precioEdificar + ", numCasas=" + numCasas + ", numHoteles=" + numHoteles + '}';
+    }
+    
+    
+    
+    
+}
