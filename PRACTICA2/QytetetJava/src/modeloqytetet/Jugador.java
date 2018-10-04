68f0179184b8c6c19b0fff57df186452bf8275fa
xexugarcia95 <xexugarcia95@gmail.com>
2018-10-04 10:34:51 +0200

    .

---
+++ PRACTICA2/QytetetJava/src/modeloqytetet/Jugador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloqytetet;
+
+/**
+ *
+ * @author jesus
+ */
+public class Jugador {
+    private boolean encarcelado;
+    private String nombre;
+    private int saldo;
+    
+    //Implementar el resto...
+}
