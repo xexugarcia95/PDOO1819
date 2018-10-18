a52f3b12a5872910baf676dc71ca67d985ae4030
xexugarcia95 <xexugarcia95@gmail.com>
2018-10-18 09:25:53 +0200

    .

--- PRACTICA2/QytetetRuby/lib/qytetet.rb
+++ PRACTICA2/QytetetRuby/lib/qytetet.rb
@@ -4,13 +4,22 @@
 # and open the template in the editor.
 
 require_relative "sorpresa.rb"
+require "singleton"
 
 module ModeloQytetet
   class Qytetet
+    include Singleton
+
+
     attr_reader :mazo, :tablero
-    
+    @@instance = Qytetet.new
     @mazo
     @tablero
+
+    def self.instance
+      return @instance
+    end
+
     def inicializar_cartas_sorpresa
         @mazo = Array.new
         inicializar_tablero
@@ -25,12 +34,156 @@ module ModeloQytetet
         mazo << Sorpresa.new("Has perdido una apuesta. Paga 20 a cada jugador.",TipoSorpresa::PORJUGADOR,-20)
         mazo << Sorpresa.new("Has quedado libre de la cárcel.",TipoSorpresa::SALIRCARCEL,0)
     end
-    
+
+    def actuarSiEnCasillaEdificable
+
+
+    end
+
+    def actuarSiEnCasillaNoEdificable
+
+    end
+
+
+    def void aplicarSorpresa
+
+    end
+
+
+    def cancelarHipoteca(numeroCasilla)
+
+        raise NotImplementedError
+    end
+
+    def comprarTituloPropiedad
+
+        raise NotImplementedError
+    end
+
+    def edificarCasa(numeroCasilla)
+
+        raise NotImplementedError
+    end
+
+    def edificarHotel(numeroCasilla)
+
+        raise NotImplementedError
+    end
+
+
+
+    def getValorDado
+
+        raise NotImplementedError
+
+
+    def hipotecarPropiedad(numeroCasilla)
+
+    end
+
+
+    def inicializarJuego(nombres)
+
+        inicializarJugadores(nombres);
+        inicializarCartasSorpresa();
+    end
+
+
+
+    def entarSalirCarcel(metodo)
+    {
+        raise NotImplementedError
+    }
+
+    def jugar()
+
+
+    end
+
+    def( numCasillaDestino)
+
+
+    end
+
+    def obtenerCasillaJugadorActual
+
+        raise NotImplementedError
+    end
+
+    def obtenerCasillasTablero
+
+        raise NotImplementedError
+    end
+
+    def obtenerPropiedadesJugador
+
+        raise NotImplementedError
+    end
+
+    def obtenerPropiedadesJugadorSegunEstadoHipoteca(estadoHipoteca)
+
+        raise NotImplementedError
+    end
+
+    def obtenerRanking
+
+
+    end
+
+    def  obtenerSaldoJugadorActual
+
+        raise NotImplementedError
+    end
+
+
+
+    def siguienteJugador
+
+
+    end
+
+    def tirarDado
+    {
+        raise NotImplementedError
+    }
+
+    def venderPropiedad( numeroCasilla)
+
+        raise NotImplementedError
+    end
+
     private
-    
+
+    def initialize()
+      nombres = Array.new
+      nombres<<"Jugador 1"
+      nombres<<"Jugador 2"
+      inicializarJuego(nombres)
+
+    end
+
     def inicializar_tablero
       @tablero = Tablero.new
     end
-    
+
+    def encarcelarJugador
+
+
+    end
+
+    def inicializarJugadores(ArrayList<String> nombres)
+
+        for( i=0;i<nombres.size();i++)
+
+            Jugador j = new Jugador(nombres.get(i));
+            jugadores.add(j);
+
+    end
+
+    def salidaJugadores
+
+
+    end
+
   end
 end
